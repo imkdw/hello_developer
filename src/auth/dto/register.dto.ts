4a7c976234c9bebfd6816bc67351bf03648d5416
Dongwoo-Kim <dongeu47@gmail.com>
2023-04-26 22:11:30 +0900

    비밀번호 제약조건 추가
    기존 : 정규식을 통한 검사
    변경 : (추가)최대 자리수 30자리

--- src/auth/dto/register.dto.ts
+++ src/auth/dto/register.dto.ts
@@ -1,31 +1,30 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsEmail, Matches, MaxLength, MinLength } from 'class-validator';
+import { IsEmail, Length, Matches, MaxLength, MinLength } from 'class-validator';
 
 export class RegisterDto {
+  @IsEmail()
   @ApiProperty({
     example: 'test@test.com',
     description: '회원가입시 사용되는 이메일 주소',
     required: true,
   })
-  @IsEmail()
   email: string;
 
+  @Length(10, 30)
+  @Matches(/[\{\}\[\]\/?.,;:|\)*~`!^\-_+<>@\#$%&\\\=\(\'\"]/g)
   @ApiProperty({
     example: 'password!@#',
     description: '회원가입시 사용되는 비밀번호',
     required: true,
   })
-  @MinLength(10)
-  @Matches(/[\{\}\[\]\/?.,;:|\)*~`!^\-_+<>@\#$%&\\\=\(\'\"]/g)
   password: string;
 
+  @Length(2, 8)
+  @Matches(/^[^{}\[\]\/?.,;:|\)*~`!^\-_+<>@\#$%&\\\=\(\'\"]*$/)
   @ApiProperty({
     example: 'testuser',
     description: '회원가입시 사용되는 닉네임',
     required: true,
   })
-  @MinLength(2)
-  @MaxLength(8)
-  @Matches(/^[^{}\[\]\/?.,;:|\)*~`!^\-_+<>@\#$%&\\\=\(\'\"]*$/)
   nickname: string;
 }
