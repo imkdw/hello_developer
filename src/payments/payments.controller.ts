384d68d4f666104bdfd06fb5d32e75e99d8691a1
imkdw <imkdw@kakao.com>
2023-06-12 21:53:00 +0900

    fix: socket 호출방식 변경, 배포시 테스트 주석처리

--- src/payments/payments.controller.ts
+++ src/payments/payments.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Post, Body } from '@nestjs/common';
 import { RequestWelpayDto } from './dto/request-welpay.dto';
 import { io } from 'socket.io-client';
+import * as https from 'https';
 
 @Controller('payments')
 export class PaymentsController {
@@ -14,8 +15,30 @@ export class PaymentsController {
     const P_MID = 'welcometst';
     const { P_TID, P_REQ_URL } = requestWelPayDto;
     const socket = io(P_REQ_URL);
-    socket.emit('message', makeParam(P_TID, P_MID));
-    socket.on('message', (data: any) => console.log(data));
+    // socket.emit('message', makeParam(P_TID, P_MID));
+
+    const options = {
+      hostname: P_REQ_URL,
+      port: 443,
+      path: '/',
+      method: 'POST',
+    };
+
+    const req = https.request(options, (res) => {
+      console.log('status: ', res.statusCode);
+
+      res.on('data', (chunk) => {
+        console.log('res: ', chunk);
+      });
+    });
+
+    req.on('error', (error) => {
+      console.error(error);
+    });
+
+    req.write(makeParam(P_TID, P_MID));
+    req.end();
+
     return JSON.stringify(requestWelPayDto);
   }
 }
