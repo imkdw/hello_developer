37901527bf839783c03664e513c3be3beddd2bf2
imkdw <imkdw@kakao.com>
2023-06-13 11:44:16 +0900

    리다이렉트 구성

---
+++ src/payments/dto/response-welpay.dtk.ts
@@ -0,0 +1,15 @@
+export class ResponseWelPayDto {
+  P_STATUS: string;
+  P_TID: string;
+  P_TYPE: string;
+  P_AUTH_DT: string;
+  P_MID: string;
+  P_OID: string;
+  P_AMT: string;
+  P_UNAME: string;
+  P_MNAME: string;
+  P_RMESG1: string;
+  P_NOTI: string;
+  P_NOTEURL: string;
+  P_NEXT_URL: string;
+}
--- src/payments/payments.controller.ts
+++ src/payments/payments.controller.ts
@@ -1,24 +1,22 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, Get, Redirect } from '@nestjs/common';
 import { RequestWelpayDto } from './dto/request-welpay.dto';
 import { io } from 'socket.io-client';
 import * as https from 'https';
 import * as iconv from 'iconv-lite';
+import { ResponseWelPayDto } from './dto/response-welpay.dtk';
 
 @Controller('payments')
 export class PaymentsController {
   @Post('nextUrl')
+  @Redirect()
   nextUrl(@Body() requestWelPayDto: RequestWelpayDto) {
-    console.log(requestWelPayDto);
     const makeParam = (P_TID: string, P_MID: string): string => {
       const param = 'P_TID=' + P_TID + '&P_MID=' + P_MID;
-      console.log('param: ', param);
       return param;
     };
 
     const P_MID = 'welcometst';
     const { P_TID, P_REQ_URL } = requestWelPayDto;
-    const socket = io(P_REQ_URL);
-    // socket.emit('message', makeParam(P_TID, P_MID));
 
     const options = {
       hostname: 'tmobile.paywelcome.co.kr',
@@ -31,17 +29,33 @@ export class PaymentsController {
       console.log('status: ', res.statusCode);
 
       res.on('data', (chunk) => {
-        console.log('res: ', iconv.decode(chunk, 'euc-kr'));
+        const parsedChunk = JSON.parse(iconv.decode(chunk, 'euc-kr'));
+        const responseWelPayDto = new ResponseWelPayDto();
+        console.log(parsedChunk);
+        responseWelPayDto.P_OID = parsedChunk.P_OID;
+        return {
+          url: `https://hdev.site/payments/result?P_OID=${responseWelPayDto.P_OID}`,
+        };
       });
     });
 
     req.on('error', (error) => {
       console.error(error);
+      return {
+        url: 'https://hdev.site/payments/result?error=true',
+      };
     });
 
     req.write(makeParam(P_TID, P_MID));
     req.end();
+  }
 
-    return JSON.stringify(requestWelPayDto);
+  @Get('test')
+  @Redirect()
+  test() {
+    console.log('리다이렉트 호출');
+    return {
+      url: 'https://hdev.site/payments/result?P_OID=tst_12421321312',
+    };
   }
 }
