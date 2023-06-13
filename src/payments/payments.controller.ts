dd2c52c91f32bf1129c277568b93b312824edadf
imkdw <imkdw@kakao.com>
2023-06-13 12:12:20 +0900

    Update payments.controller.ts

--- src/payments/payments.controller.ts
+++ src/payments/payments.controller.ts
@@ -8,8 +8,8 @@ import { ResponseWelPayDto } from './dto/response-welpay.dtk';
 @Controller('payments')
 export class PaymentsController {
   @Post('nextUrl')
-  @Redirect()
-  nextUrl(@Body() requestWelPayDto: RequestWelpayDto) {
+  @Redirect() // Redirect 데코레이터 사용
+  async nextUrl(@Body() requestWelPayDto: RequestWelpayDto) {
     const makeParam = (P_TID: string, P_MID: string): string => {
       const param = 'P_TID=' + P_TID + '&P_MID=' + P_MID;
       return param;
@@ -25,58 +25,55 @@ export class PaymentsController {
       method: 'POST',
     };
 
-    const req = https.request(options, (res) => {
-      console.log('status: ', res.statusCode);
+    return new Promise((resolve, reject) => {
+      const req = https.request(options, (res) => {
+        console.log('status: ', res.statusCode);
 
-      res.on('data', (chunk) => {
-        const parsedChunk = iconv.decode(chunk, 'euc-kr');
-        console.log(parsedChunk);
+        res.on('data', (chunk) => {
+          const parsedChunk = iconv.decode(chunk, 'euc-kr');
+          console.log(parsedChunk);
 
-        const params = new URLSearchParams(parsedChunk);
-        const data: ResponseWelPayDto = {
-          P_STATUS: params.get('P_STATUS') || '',
-          P_TID: params.get('P_TID') || '',
-          P_TYPE: params.get('P_TYPE') || '',
-          P_AUTH_DT: params.get('P_AUTH_DT') || '',
-          P_MID: params.get('P_MID') || '',
-          P_OID: params.get('P_OID') || '',
-          P_AMT: params.get('P_AMT') || '',
-          P_UNAME: params.get('P_UNAME') || '',
-          P_MNAME: params.get('P_MNAME') || '',
-          P_RMESG1: params.get('P_RMESG1') || '',
-          P_NOTI: params.get('P_NOTI') || '',
-          P_NOTEURL: params.get('P_NOTEURL') || '',
-          P_NEXT_URL: params.get('P_NEXT_URL') || '',
-        };
+          const params = new URLSearchParams(parsedChunk);
+          const data: ResponseWelPayDto = {
+            P_STATUS: params.get('P_STATUS') || '',
+            P_TID: params.get('P_TID') || '',
+            P_TYPE: params.get('P_TYPE') || '',
+            P_AUTH_DT: params.get('P_AUTH_DT') || '',
+            P_MID: params.get('P_MID') || '',
+            P_OID: params.get('P_OID') || '',
+            P_AMT: params.get('P_AMT') || '',
+            P_UNAME: params.get('P_UNAME') || '',
+            P_MNAME: params.get('P_MNAME') || '',
+            P_RMESG1: params.get('P_RMESG1') || '',
+            P_NOTI: params.get('P_NOTI') || '',
+            P_NOTEURL: params.get('P_NOTEURL') || '',
+            P_NEXT_URL: params.get('P_NEXT_URL') || '',
+          };
 
-        for (const [key, value] of params.entries()) {
-          data[key] = value;
-        }
-        const responseWelPayDto = new ResponseWelPayDto();
-        responseWelPayDto.P_OID = data.P_OID;
-        return {
-          url: `https://hdev.site/payments/result?P_OID=${responseWelPayDto.P_OID}`,
-        };
+          for (const [key, value] of params.entries()) {
+            data[key] = value;
+          }
+
+          const responseWelPayDto = new ResponseWelPayDto();
+          responseWelPayDto.P_OID = data.P_OID;
+
+          // 리다이렉트할 URL 반환
+          resolve({
+            url: `https://hdev.site/payments/result?P_OID=${responseWelPayDto.P_OID}`,
+          });
+        });
       });
+
+      req.on('error', (error) => {
+        console.error(error);
+        // 에러 발생 시 리다이렉트할 URL 반환
+        reject({
+          url: 'https://hdev.site/payments/result?error=true',
+        });
+      });
+
+      req.write(makeParam(P_TID, P_MID));
+      req.end();
     });
-
-    req.on('error', (error) => {
-      console.error(error);
-      return {
-        url: 'https://hdev.site/payments/result?error=true',
-      };
-    });
-
-    req.write(makeParam(P_TID, P_MID));
-    req.end();
-  }
-
-  @Get('test')
-  @Redirect()
-  test() {
-    console.log('리다이렉트 호출');
-    return {
-      url: 'https://hdev.site/payments/result?P_OID=tst_12421321312',
-    };
   }
 }
