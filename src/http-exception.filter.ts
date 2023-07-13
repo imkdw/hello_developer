83fbd6a180ac097ef925c0c7be9da6c329099699
imkdw <imkdw@kakao.com>
2023-07-13 23:33:25 +0900

    파이프 유닛테스트 활성화

--- src/http-exception.filter.ts
+++ src/http-exception.filter.ts
@@ -12,7 +12,6 @@ const getKoreaTime = () => {
   const day = String(date.getDate()).padStart(2, '0');
   const hour = String(date.getHours()).padStart(2, '0');
   const minute = String(date.getMinutes()).padStart(2, '0');
-  const second = String(date.getSeconds()).padStart(2, '0');
 
   return `${year}-${month}-${day} ${hour}:${minute}`;
 };
